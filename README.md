d914d9bba938fbaa9288836a8d20d930192802a8
r3kaushi <r3kaushi@uwaterloo.ca>
2023-05-12 16:42:28 +0200

    cranfield

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Example search application for OpenWebSearch. Created using Lucene and JS.
 
 ### CIFF Import & Export
 - First, make a CIFF index by using an export (for example, from Anserini) by following the instructions in this README: https://github.com/informagi/lucene-ciff 
-- From the above repository, run the application to import the CIFF index to Lucene.
+- From the above repository, run the application to import the CIFF index to a Lucene index directory.
 
 ### Create Application
 - With the imported Lucene index, a search application can be made (such as in this repository) in any language.
---
+++ search-app/src/Components/Search.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { search } from 'lucene';
+
+function Search() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+  
+    const handleSearch = () => {
+      const indexPath = '../../cranfield';
+      const queryString = query;
+  
+      const results = search(indexPath, queryString);
+  
+      setResults(results);
+    };
+  
+    return (
+      <div>
+        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={handleSearch}>Search</button>
+        <ul>
+            {results}
+          {/* {results.map((result) => (
+            <li key={result.id}>
+              <h2>{result.title}</h2>
+              <p>{result.content}</p>
+              <a href={result.url}>View Document</a>
+            </li>
+          ))} */}
+        </ul>
+      </div>
+    );
+}
+
+export default Search;
